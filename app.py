50710760abf77189447f14e1d90abb9ff4e8c821
Salman <maker57sk@gmail.com>
2022-06-23 03:06:17 +0400

    streamlit docker basic template app created

---
+++ app.py
@@ -0,0 +1,22 @@
+# streamlit app basic template with docker 
+import streamlit as st
+
+
+
+
+
+
+def main():
+    st.write('Welcome to streamlit with docker')
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
