5a023400b093faf0bceeb61ee750a5bb656ee580
Salman <maker57sk@gmail.com>
2022-06-23 03:22:51 +0400

    configration settings added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # streamlit app basic template with docker 
 import streamlit as st
 
-
-
-
+st.set_page_config(
+     page_title="Linkedin",
+     page_icon='✌️',
+     layout="wide",
+     initial_sidebar_state="expanded")
 
 
 def main():
