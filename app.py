56e7e3195f95fc70f05d9242f1d4fb746237134f
Salman <55950919+maker57sk@users.noreply.github.com>
2022-06-29 12:40:39 +0400

    name changes

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 import streamlit as st
 
 st.set_page_config(
-     page_title="Linkedin",
+     page_title="Streamlit App",
      page_icon='✌️',
      layout="wide",
      initial_sidebar_state="expanded")
 
 
 def main():
-    st.write('Welcome to streamlit with docker')
+    st.write('Welcome to Streamlit in Docker')
 
 
 
